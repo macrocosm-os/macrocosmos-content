2e942cbb13e8dea2bc8a2b219eb9d1f4f001301f
Lawrence Hunt <huntlawrence@gmail.com>
2024-12-18 18:04:47 +0000

    add all subnets as folders

--- src/pages/_meta.ts
+++ src/pages/_meta.ts
@@ -2,4 +2,9 @@ export default {
   index: {
     // this forces the index file to be the first one in the sidebar
   },
+  "subnet-1": "Subnet 1",
+  "subnet-9": "Subnet 9",
+  "subnet-13": "Subnet 13",
+  "subnet-25": "Subnet 25",
+  "subnet-37": "Subnet 37",
 };
