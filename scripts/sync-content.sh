4c86789bcff0d6ba7dba5e2b809c4889b7bf947a
Lawrence Hunt <huntlawrence@gmail.com>
2025-03-10 14:39:20 +0700

    tweak comments in sync-content.sh

--- scripts/sync-content.sh
+++ scripts/sync-content.sh
@@ -4,11 +4,12 @@
 # Vendir creates a folder and syncs it with the specified folder in the remote repo.
 # This script then moves that content to the root of the src/pages directory, before removing the empty content directory.
 
-# Sync content from remote repository
+# Sync content from remote repositories to a new folder called temporary-content
+# as specified in vendir.yml
 vendir sync
 
-# Move folding files to src/pages/subnet-25
+# Move files from temporary-content subfolders to their relevant place in the actual content folder structure
 mv temporary-content/finetuning/docs/* main-content/subnets/subnet-37-finetuning/
 
-# Clean up empty content directory
+# Clean up temporary-content directory
 rm -r temporary-content
